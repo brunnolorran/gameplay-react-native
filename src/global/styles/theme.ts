f24e3f40bb70d0198a362c129c22b9052bc5b45a
Brunno Lorran <brunnolorran.silva@gmail.com>
2022-05-19 20:52:21 -0300

    feat: created SignIn and initial folders essencial

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+    colors: {
+        primary: '#E51C44',
+        background: '#0D133D',
+        heading: '#DDE3F0',
+        body: '#ABB1CC',
+        line: '#991F36'
+    }
+}
